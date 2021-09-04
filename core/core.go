527bd72365a0e2224b6c001154bd42903defd7dc
Sepehr Mohammadi <mohamadi.sepehr@live.com>
2021-09-05 02:02:43 +0430

    now watches pods status instead of polling

--- core/core.go
+++ core/core.go
@@ -11,18 +11,22 @@ import (
 	"github.com/thesepehrm/kubercord/config"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
 var (
-	errNoMatches = errors.New("No matches")
+	errNoMatches    = errors.New("No matches")
+	importantPhases = []string{"FailedScheduling", "FailedSync", "FailedValidation", "Failed", "Succeeded", "Running", "Completed", "Pending"}
 )
 
 type KuberCord struct {
-	clientset *kubernetes.Clientset
-	dh        *DiscordHandler
-	q         chan struct{}
+	clientset   *kubernetes.Clientset
+	dh          *DiscordHandler
+	podsWatcher watch.Interface
+	podsCache   map[string]*v1.Pod
+	q           chan struct{}
 }
 
 func Init() (*KuberCord, error) {
@@ -48,7 +52,13 @@ func Init() (*KuberCord, error) {
 }
 
 func (kc *KuberCord) Start() {
+	var err error
+	kc.podsWatcher, err = kc.clientset.CoreV1().Pods(config.Config.K8s.Namespace).Watch(context.Background(), metav1.ListOptions{})
+	if err != nil {
+		logrus.WithError(err).Panic("Failed to watch pods")
+	}
 	go kc.Watch()
+	go kc.watchForEvents()
 }
 
 func (kc *KuberCord) Stop() {
@@ -60,6 +70,7 @@ func (kc *KuberCord) Watch() {
 	for {
 		select {
 		case <-kc.q:
+			kc.podsWatcher.Stop()
 			return
 		default:
 			kc.watch()
@@ -67,6 +78,14 @@ func (kc *KuberCord) Watch() {
 		time.Sleep(time.Duration(config.Config.K8s.Interval) * time.Second)
 	}
 }
+
+func (kc *KuberCord) watchForEvents() {
+	for event := range kc.podsWatcher.ResultChan() {
+		pod := event.Object.(*v1.Pod)
+		kc.handlePod(pod)
+	}
+}
+
 func (kc *KuberCord) watch() {
 	n := config.Config.K8s.Namespace
 	s := int64(config.Config.K8s.Interval)
@@ -98,14 +117,6 @@ func (kc *KuberCord) watch() {
 					}).Error("Failed to send alert")
 			}
 		}
-		if string(pod.Status.Phase) != "Running" && string(pod.Status.Phase) != "Completed" {
-			logrus.WithFields(logrus.Fields{
-				"pod":    pod.Name,
-				"status": pod.Status.Phase,
-				"reason": pod.Status.Reason,
-			}).Error("Pod failed")
-			kc.dh.SendPodStatus(pod.Name, string(pod.Status.Phase), pod.Status.Reason)
-		}
 	}
 }
 
@@ -131,9 +142,9 @@ func parseLogs(service, logs string) *Alert {
 		if params.Level == "error" {
 			return NewAlert(service, Error, params.Msg, params.Time, splitted)
 		}
-		if params.Level == "warn" {
+		/*if params.Level == "warn" {
 			return NewAlert(service, Warn, params.Msg, params.Time, splitted)
-		}
+		}*/
 		/*if params.Level == "info" {
 			return NewAlert(service, Info, params.Msg, params.Time, splitted)
 		}
@@ -167,5 +178,27 @@ func extractParams(line string) (*Params, error) {
 		Level: matches[2],
 		Msg:   matches[3],
 	}, nil
+
+}
+
+func (kc *KuberCord) handlePod(newPod *v1.Pod) {
+	if p, ok := kc.podsCache[newPod.Name]; ok {
+		if p.Status.Phase == newPod.Status.Phase {
+			return
+		}
+	}
+
+	kc.podsCache[newPod.Name] = newPod
+
+	if arrayContains(importantPhases, string(newPod.Status.Phase)) {
+		logrus.WithFields(logrus.Fields{
+			"pod":    newPod.Name,
+			"status": newPod.Status.Phase,
+		}).Info("Pod status changed")
+		err := kc.dh.SendPodStatus(newPod.Name, string(newPod.Status.Phase), newPod.Status.Message)
+		if err != nil {
+			logrus.WithError(err).Error("Failed to send pod status")
+		}
+	}
 
 }
