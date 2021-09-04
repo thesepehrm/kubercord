7809043d98eb1b1f28beb4cbb9bfb4b6c44665c4
Sepehr Mohammadi <mohamadi.sepehr@live.com>
2021-09-05 02:07:23 +0430

    moved util functions

--- core/util.go
+++ core/util.go
@@ -1,5 +1,10 @@
 package core
 
+import (
+	"regexp"
+	"time"
+)
+
 func arrayContains(array []string, value string) bool {
 	for _, v := range array {
 		if v == value {
@@ -7,4 +12,30 @@ func arrayContains(array []string, value string) bool {
 		}
 	}
 	return false
+}
+
+type Params struct {
+	Time  time.Time
+	Level string
+	Msg   string
+}
+
+func extractParams(line string) (*Params, error) {
+	r := regexp.MustCompile(`time="(.*)"\s+level=(.*)\s+msg="(.*)"`)
+	matches := r.FindStringSubmatch(line)
+	if len(matches) == 0 {
+		return nil, errNoMatches
+	}
+
+	t, err := time.Parse("2006-01-02T15:04:05Z", matches[1])
+	if err != nil {
+		return nil, err
+	}
+
+	return &Params{
+		Time:  t,
+		Level: matches[2],
+		Msg:   matches[3],
+	}, nil
+
 }
